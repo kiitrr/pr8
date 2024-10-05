f79f837e37b871ec78d60816ec4965a8353dbc60
kiitrr <renatuz10@gmail.com>
2024-10-05 14:01:04 +0500

    Update pr83.py

--- homework3/pr83.py
+++ homework3/pr83.py
@@ -3,10 +3,9 @@ def draw_square(size):
     for _ in range(size):
         print(' * ' * size)
 
-# Ввод размера квадрата
 size_input = input("Введите размер квадрата (целое число): ")
 
-# Проверка на целое число
+
 if size_input.isdigit() and int(size_input) == 10:
     size = int(size_input)
     draw_square(size)
