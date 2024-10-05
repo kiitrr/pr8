e18ac2e9b0e59bf55725fe8d1dacff69ae463735
kiitrr <renatuz10@gmail.com>
2024-10-05 13:36:39 +0500

    Update rr.py

--- homework5/rr.py
+++ homework5/rr.py
@@ -1,1 +1,10 @@
+def is_integer(n):
+    try:
+        float_n = float(n)
+        return float_n.is_integer()
+    except ValueError:
+        return False
 
+
+a = input("Введите первое число (a): ")
+b = input("Введите второе число (b): ")
