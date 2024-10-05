67f820599996001be8b5f43d422117b67247b665
kiitrr <renatuz10@gmail.com>
2024-10-05 13:37:02 +0500

    Update rr.py

--- homework5/rr.py
+++ homework5/rr.py
@@ -1,3 +1,4 @@
+
 def is_integer(n):
     try:
         float_n = float(n)
@@ -13,3 +14,13 @@ b = input("Введите второе число (b): ")
 if is_integer(a) and is_integer(b):
     a = int(a)
     b = int(b)
+
+    
+    start = min(a, b) + 1
+    end = max(a, b)
+
+    
+    for number in range(start, end):
+        print(number)
+else:
+    print("Ошибка: оба введенных числа должны быть целыми.")
